208cf9e0f55da01e806f85908832d0dbb1dc07be
rafie <rafie@avaya.com>
2015-07-25 17:38:22 +0300

    ...

--- Confetti/db/migrate/00001_schema001.rb
+++ Confetti/db/migrate/00001_schema001.rb
@@ -3,6 +3,8 @@ require 'active_record'
 
 class Schema001 < ActiveRecord::Migration
 	def up
+		#--------------------------------------------------------------------------------------
+
 		create_table :views do |t|
 			t.column :name, :string
 			t.column :user, :string
@@ -10,6 +12,8 @@ class Schema001 < ActiveRecord::Migration
 		end
 
 		add_index :views, :name, unique: true
+
+		#--------------------------------------------------------------------------------------
 
 		create_table :activities do |t|
 			t.column :name, :string    
@@ -23,6 +27,8 @@ class Schema001 < ActiveRecord::Migration
 
 		add_index :activities, :name, unique: true
 
+		#--------------------------------------------------------------------------------------
+
 		create_table :checks do |t|
 			t.column :name, :string
 			t.column :user, :string
@@ -31,12 +37,16 @@ class Schema001 < ActiveRecord::Migration
 
 		add_index :checks, :name, unique: true
 
+		#--------------------------------------------------------------------------------------
+
 		create_table :projects do |t|
 			t.column :name, :string
 			t.column :branch, :string
 		end
 
 		add_index :projects, :name, unique: true
+
+		#--------------------------------------------------------------------------------------
 
 		create_table :project_versions do |t|
 			t.column :project_id, :integer
@@ -46,11 +56,15 @@ class Schema001 < ActiveRecord::Migration
 
 		add_index :project_versions, [:project_id, :version], unique: true
 
+		#--------------------------------------------------------------------------------------
+
 		create_table :products do |t|
 			t.column :name, :string
 		end
 
 		add_index :products, :name, unique: true
+
+		#--------------------------------------------------------------------------------------
 
 		create_table :product_versions do |t|
 			t.column :product_id, :integer
@@ -58,5 +72,7 @@ class Schema001 < ActiveRecord::Migration
 		end
 		
 		add_index :product_versions, [:product_id, :name], unique: true
+
+		#--------------------------------------------------------------------------------------
 	end
 end
--- Confetti/db/migrate/00002_schema002.rb
+++ Confetti/db/migrate/00002_schema002.rb
@@ -3,13 +3,19 @@ require 'active_record'
 
 class Schema002 < ActiveRecord::Migration
 	def up
+		#--------------------------------------------------------------------------------------
+
 		add_column :views, :nick, :string
 
 		add_index :views, :nick, unique: false
+
+		#--------------------------------------------------------------------------------------
 
 		add_column :activities, :nick, :string
 
 		add_index :activities, :nick, unique: false
 		add_index :activities, :user, unique: false
+
+		#--------------------------------------------------------------------------------------
 	end
 end
